4da10ecf4a74ce70c71930861f5fe8cdb2184055
Olukayode <olukayodeshokunbi@gmail.com>
2021-01-19 06:32:46 -0500

    Completed function that returns a randonmly selected destination from an array of destnations

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function randomNum() {
   let number = Math.trunc(Math.random() * 9) + 1;
   return number;
 }
+
+// DESTINATION Function that returns a randomly genenrated destination
+function destination(num) {
+  let destinationsArray = [
+    "Milan",
+    "Paris",
+    "Los Angeles",
+    "Dubai",
+    "Sao Paolo",
+    "New York",
+    "Durban",
+    "London",
+    "Zurich",
+    "Marekesh",
+  ];
+  return destinationsArray[num];
+}
