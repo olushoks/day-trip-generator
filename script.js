a0a4fa9b0cefee1a8cc1fdf0adceeb000c3393ec
Olukayode <olukayodeshokunbi@gmail.com>
2021-01-19 06:21:30 -0500

    Created function that generates and returns a random number that matches values for destinations, restaurants, means of transportation, and forms of transportation

---
+++ script.js
@@ -0,0 +1,6 @@
+/************************ DAY TRIP GENERATOR ********************/
+// Function that generates RANDOM number
+function randomNum() {
+  let number = Math.trunc(Math.random() * 9) + 1;
+  return number;
+}
